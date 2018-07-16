d2f19b42b47fcb8ad5514947474d9b854558c29a
antoinecaron.simplon <antoinecaron.simplon@gmail.com>
2018-07-16 11:26:25 +0200

    commit

--- hotel/js/app.js
+++ hotel/js/app.js
@@ -9,22 +9,5 @@
 
     $('#site-cache').click(function(e){
         $('body').removeClass('with--sidebar');
-    })
-
-    // Footer function
-
-    // $('#footer__icon').click(function(e){
-    //     e.preventDefault();
-    //     $('body').toggleClass('with--sidebar');
-    // });  
-
-    // $( '#header__icon' ).click(function() {
-    //     $( 'body' ).toggleClass( 'with--sidebar' );
-    //   });
-    
-    // $( '#footer__icon' ).click(function() {
-    //     $( 'body' ).toggleClass( 'with--sidebar' );
-    //   });
-
-    
+    })    
 })(jQuery);
